3fd6ee30a4f42dca0ca11c8293589b9c1c80c3dc
ZhanbolotAbdrasilov17 <itpythonzhanbolot@gmail.com>
2023-02-06 07:30:29 +0600

    update

--- main.js
+++ main.js
@@ -6,84 +6,90 @@ const pics = [
   'https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
   'https://images.unsplash.com/photo-1627483262112-039e9a0a0f16?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
   'https://images.unsplash.com/photo-1469334031218-e382a71b716b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
-  'https://images.unsplash.com/photo-1674745843370-244462ab8bb6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+  'https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1020&q=80',
 ]
 
 
-const images = document.querySelector('.images')
+const gallery = document.querySelector('.gallery')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close')
 
-pics.forEach((picture) => {
-  const img = document.createElement('img')
-  img.setAttribute('src', './img/Group 1.png')
-  images.append(img)
-  img.setAttribute('alt', 'porfolio')
-  // img.setAttribute('src', picture)
-
-  // img.addEventListener('load', () => {
-  //   img.setAttribute('src', picture)
-  //   console.log(1)
-  // })
-
-  setTimeout(() => {
-    img.setAttribute('src', picture)
-  }, 1000)
-
-})
-
-
-
-const img = Array.from(document.querySelectorAll('.images>img'))
-const pictureModal = document.querySelector('.picture-modal')
-const pictureOverlay = document.querySelector('.picture-overlay')
-const closePicture = document.querySelector('.close-picture')
-
-img.forEach((picture) => {
-  picture.addEventListener('click', () => {
-    const liveImg = document.createElement('img')
-
-    liveImg.setAttribute('src', picture.getAttribute('src'))
-    pictureModal.prepend(liveImg)
-
-    pictureOverlay.classList.add('openPicture')
-  })
-
-})
-
-closePicture.addEventListener('click', () => {
-  pictureOverlay.classList.remove('openPicture')
-  pictureModal.removeChild(document.querySelector('.picture-modal>img'))
-})
-
-
-
-const openContactReg = document.querySelector('.contact')
-const closeContactReg = document.querySelector('#close-contact')
-const contactOverlay = document.querySelector('.contact-overlay')
-
-openContactReg.addEventListener('click', () => {
-  contactOverlay.classList.add('openContact')
-})
-
-
-closeContactReg.addEventListener('click', () => {
-  contactOverlay.classList.remove('openContact')
-
-})
-
-
-const send = document.querySelector('.contact-modal-button')
-
-let data = {
-  name: '',
-  email: '',
-  massage: ''
+const createGalleryItem = () => {
+  const div = document.createElement('div')
+  div.classList.add('image-overlay')
+  return div 
 }
 
-send.addEventListener('click', () => {
-  let { name, email, message } = data
-  name = document.querySelector('#input-name').value
-  email = document.querySelector('#input-email').value
-  message = document.querySelector('#text-area').value
-  console.log(data)
-  contactOverlay.classList.remove('openContact')
+const createImg = (src) => {
+  const img = document.createElement('img')
+  img.setAttribute('src', src)
+  return img
+}
+
+pics.forEach((imgSrc) => {
+  const home = createGalleryItem(imgSrc)
+
+  const imgForGallery = createImg(imgSrc)
+  const imgForModal = createImg(imgSrc)
+
+  const child = document.createElement('div')
+  child.className = 'child'
+
+  const svjImage = createImg('./image/lupa.svg')
+  child.append(svjImage)  
+
+  child.addEventListener('click', () => {
+    overlay.classList.add('open')
+    modal.append(imgForModal)
+  })
+
+  gallery.append(home)
+  home.append(imgForGallery, child)
+})
+
+closeBtn.addEventListener('click', () => {
+  overlay.classList.remove('open')
+  modal.querySelector('img').remove()
+})
+
+
+
+
+
+const contactOpen = document.querySelector('.contact')
+const contactClose = document.querySelector('.close-cross')
+const contactModal = document.querySelector('.contact-modal')
+const overlayContact = document.querySelector('.overlay-contact')
+
+const body = document.querySelector('body')
+
+contactOpen.addEventListener('click', () => {
+  overlayContact.classList.add('open')
+  body.appendChild(overlayContact)
+  overlayContact.appendChild(contactModal)
+})
+
+contactClose.addEventListener('click', () => {
+  overlayContact.classList.remove('open')
+  document.body.removeChild(overlayContact)
+  overlayContact.removeChild(contactModal)
+})
+
+
+const sendMessage = document.querySelector('.send')
+const inputText = document.querySelector('input[type="text"]')
+const inputEmail = document.querySelector('input[type="email"]')
+const textArea = document.querySelector('textarea')
+
+const user = {}
+sendMessage.addEventListener('click', () => {
+  user.name = inputText.value
+  user.email = inputEmail.value
+  user.message = textArea.value
+  console.log(user)
+
+  overlayContact.classList.remove('open')
+  document.body.removeChild(overlayContact)
+  overlayContact.removeChild(contactModal)  
 })
